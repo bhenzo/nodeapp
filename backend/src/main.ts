d48df013648aa285f261a5c0f7e5b1553146ec02
Enzo <scalone.enzo@gmail.com>
2023-07-22 21:14:23 -0300

    fixxxx

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const isDev = process.env.NODE_ENV === "development";
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
-  await app.listen(3007);
+  await app.listen(isDev ? 3007 : 8080);
 }
 bootstrap();
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+\. "$HOME/.nvm/nvm.sh"
+\. "$NVM_DIR/bash_completion"
 cd /home/ec2-user/nodeapp/dist
 screen -dmS nodeapp node main.js
