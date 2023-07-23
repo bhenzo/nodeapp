86a5389f9462e0499dc69eefbfc5f2bfe810c362
Enzo <scalone.enzo@gmail.com>
2023-07-22 21:09:38 -0300

    fixes

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+\. "$HOME/.nvm/nvm.sh"
+\. "$NVM_DIR/bash_completion"
 cd /home/ec2-user/nodeapp
 sudo chmod 777 -R .
-/home/ec2-user/.nvm/versions/node/v16.20.1/bin/npm install
-/home/ec2-user/.nvm/versions/node/v16.20.1/bin/npm run build
+npm install
+npm run build
 sudo chmod 777 -R dist
 
